3139fd6af6b65026e9a4e974dfdd8f3c9ca6654e
chendachao <dachao.chen@accenture.com>
2016-07-10 15:18:12 +0800

    add dev

---
+++ app/features/step_definitions/settings.steps.js
@@ -0,0 +1,88 @@
+module.exports = function () {
+    
+    this.Given(/^I go to ACAT's home page$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.Given(/^there will be a welcome popup$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I close the popup$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^it will check whether I have access permission$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I have permission$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.Then(/^there will be a popup to let me assign model$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I choose to display the model view with one of the below plans:$/, function (table, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^even countries if I choose "([^"]*)" plan:$/, function (arg1, table, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^currencies:$/, function (table, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^submit my selection$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.Then(/^the model view will be displayed as I assigned$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^already choose a plan type$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I want to move to a different plan$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I click the setting icon at the up right corner$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.When(/^I don't have access permission$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.Then(/^there will be alert popup$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+    this.Then(/^freeze the page$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback();
+    });
+    
+};
--- app/index.js
+++ app/index.js
@@ -1,26 +1,37 @@
-var mathsSteps = require('./features/step_definitions/maths.decrement.steps');
-var mathsFeature = require('./features/maths.decrement.feature');
+var featureSource = require('./features/settings.feature');
+var supportCode = require('./features/step_definitions/settings.steps');
 var ansi_up = require('ansi_up');
+
+// function requireAll(r) { r.keys().forEach(r); }
+// requireAll(require.context('./features/', true, /\.feature$/));
+
+// console.log(requireAll(require.context('./features/', true, /\.feature$/)));
+// console.log(require.context('.', true, /^[a-z]*/));
+// console.log(require('fs'));
+
+var options = {
+    strict: false,
+
+    // tagged with @frontend or @only but not @skip
+    tags: ['@frontend,@only', '~@skip'],
+
+    // tagged with @complex ut not @skip
+    // tags: ['~@skip', '@complex'],
+
+    // tagged with @frontend and @complex
+    // tags: ['@frontend', '@complex'],
+    backtrace: true
+};
 
 
 function runFeature() {
     var $output = $('#output');
     $output.empty();
 
-    var options = {
-        strict: false,
-        tags: ['@frontend'],
-        // tags: ['~@easy', '@complex'],
-        // tags: ['@frontend', '@complex'],
-        backtrace: true
-    };
-
-    var cucumber = Cucumber(mathsFeature, mathsSteps, options);
-    debugger
+    var cucumber = new Cucumber(featureSource, supportCode, options);
 
     var prettyFormatterOptions = {
         logToFunction: function(data) {
-            debugger
             var text = ansi_up.ansi_to_text(data);
 
             data = ansi_up.ansi_to_html(data);
@@ -39,6 +50,7 @@ function runFeature() {
             debugger
         });
     } catch(err) {
+        debugger
         var errorContainer = $('<div>');
         errorContainer.addClass('error').text(err.stack);
         $output.append(errorContainer);
@@ -47,4 +59,6 @@ function runFeature() {
 
 $(function() {
     $('#run-feature').click(runFeature);
+
+    runFeature();
 });
